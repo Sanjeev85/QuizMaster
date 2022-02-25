64e9578276630e1680047dbc5eff24b143ce20a9
Sanjeev <sanjeevchoubey215@gmail.com>
2022-02-25 12:22:12 +0530

    FireStore integration

--- app/src/main/java/com/example/quizmaster/activities/MainActivity.kt
+++ app/src/main/java/com/example/quizmaster/activities/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.quizmaster.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.quizmaster.R
 import com.example.quizmaster.adapters.quizAdapter
 import com.example.quizmaster.models.Quiz
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fireStore: FirebaseFirestore
     lateinit var quizAdapter: quizAdapter
     private var quizList = ArrayList<Quiz>()
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
@@ -19,8 +23,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        populateDummyData()
+//        populateDummyData()
         setUpViews()
+        setUpFireStore()
     }
 
 
@@ -37,6 +42,24 @@ class MainActivity : AppCompatActivity() {
         setUpDrawer()
         setUpRecyclerView()
     }
+
+    private fun setUpFireStore() {
+        fireStore = FirebaseFirestore.getInstance()
+        val collectionsReference = fireStore.collection("quizzes")
+        collectionsReference.addSnapshotListener { value, error ->
+            if (value == null || error != null) {
+                Toast.makeText(this, "Error fetching data", Toast.LENGTH_SHORT).show()
+                Log.e("ErrorFetching", "Error getting data")
+                return@addSnapshotListener
+            }
+            Log.e("Done here"," What to do now ???")
+            Log.e("Chinga", value.toObjects(Quiz::class.java).toString())
+            quizList.clear()
+            quizList.addAll(value.toObjects(Quiz::class.java))
+            quizAdapter.notifyDataSetChanged()
+        }
+    }
+
 
     fun setUpRecyclerView() {
         quizAdapter = quizAdapter(this, quizList)
---
+++ app/src/main/java/com/example/quizmaster/activities/questionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizmaster.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizmaster.R
+
+class questionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
